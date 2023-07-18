9d274eb87078e88dcf8138d86462222d9b15b015
strict-flower <126295255+strict-flower@users.noreply.github.com>
2023-07-19 05:11:10 +0900

    Get API-data from the web-site

---
+++ src/NicoVideo.rs
@@ -0,0 +1,102 @@
+use reqwest::{Client, Error, Response};
+use reqwest_cookie_store::{CookieStore, CookieStoreMutex};
+use scraper::{ElementRef, Html, Selector};
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, BufWriter};
+use std::path::Path;
+use std::sync::Arc;
+
+pub struct NicoVideo<'a> {
+    client: Client,
+    cookies_path: &'a Path,
+    cookies: Arc<CookieStoreMutex>,
+}
+
+impl<'a> NicoVideo<'a> {
+    pub fn new(cookies_path: &'a Path) -> Result<NicoVideo<'a>, io::Error> {
+        let cookies = {
+            if !cookies_path.exists() {
+                Ok::<CookieStore, io::Error>(CookieStore::new(None))
+            } else {
+                let reader = File::open(cookies_path).map(BufReader::new)?;
+                Ok(CookieStore::load_json(reader).unwrap())
+            }
+        }?;
+
+        let cookies_mutex = CookieStoreMutex::new(cookies);
+        let cookies_arc = Arc::new(cookies_mutex);
+
+        Ok(NicoVideo {
+            client: Client::builder()
+                .cookie_provider(Arc::clone(&cookies_arc))
+                .build()
+                .unwrap(),
+            cookies_path,
+            cookies: cookies_arc,
+        })
+    }
+
+    pub async fn login(self: &NicoVideo<'a>, username: &str, password: &str) -> Result<(), Error> {
+        let form_data = vec![("mail", username), ("password", password)];
+        self.post("https://secure.nicovideo.jp/secure/login", &form_data)
+            .await?;
+        self.save_cookie().unwrap();
+        Ok(())
+    }
+
+    pub async fn is_login(self: &NicoVideo<'a>) -> Result<bool, Error> {
+        let raw_html = self.get_raw_html("https://www.nicovideo.jp/my").await?;
+        Ok(!raw_html.contains("\"login_status\":\"not_login\""))
+    }
+
+    pub async fn get_video_api_data(
+        self: &NicoVideo<'a>,
+        video_id: &str,
+    ) -> Result<Option<String>, Error> {
+        let video_url = format!("https://www.nicovideo.jp/watch/{}", video_id);
+        let raw_html = self.get_raw_html(video_url.as_str()).await?;
+        let html = Html::parse_fragment(raw_html.as_str());
+        let selector = Selector::parse("div#js-initial-watch-data").unwrap();
+        if let Some(selected) = html.select(&selector).next() {
+            let elem = &selected.value();
+            let api_data = elem.attr("data-api-data").unwrap();
+            println!("{:?}", api_data);
+
+            Ok(Some(api_data.to_owned()))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn save_cookie(self: &NicoVideo<'a>) -> Result<(), io::Error> {
+        let mut writer = File::create(self.cookies_path).map(BufWriter::new)?;
+        let store = self.cookies.lock().unwrap();
+        store.save_json(&mut writer).unwrap();
+        Ok(())
+    }
+
+    async fn get(self: &NicoVideo<'a>, url: &str) -> Result<Response, Error> {
+        let res = self.client.get(url).send().await?;
+        Ok(res)
+    }
+
+    async fn get_raw_html(self: &NicoVideo<'a>, url: &str) -> Result<String, Error> {
+        let raw_html = self.get(url).await?.text().await?;
+        Ok(raw_html)
+    }
+
+    async fn post(
+        self: &NicoVideo<'a>,
+        url: &str,
+        data: &Vec<(&str, &str)>,
+    ) -> Result<Response, Error> {
+        let ret = self.client.post(url).form(&data).send().await?;
+        Ok(ret)
+    }
+
+    async fn post_raw_html(self: &NicoVideo<'a>, url: &str) -> Result<String, Error> {
+        let raw_html = self.get(url).await?.text().await?;
+        Ok(raw_html)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use reqwest::{Client, Error};
+use reqwest::Error;
+use std::env;
+use std::path::Path;
+use std::process;
+use std::sync::Arc;
+
+mod NicoVideo;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = Client::new();
-    let raw_html = client.get("https://www.nicovideo.jp/my").send().await?.text().await?;
-    if raw_html.contains('"login_status":"not_login"') {
+    use crate::NicoVideo::NicoVideo as NV;
+    let USERNAME = env::var("NV_USERNAME").unwrap_or("dummy".to_owned());
+    let PASSWORD = env::var("NV_PASSWORD").unwrap_or("dummy".to_owned());
+
+    let targets = vec!["sm29247339"];
+    let cookies_path = Path::new("cookies.json");
+    let mut nv = NV::new(cookies_path).unwrap();
+    if !nv.is_login().await? {
+        println!("[+] Need login");
+        nv.login(&USERNAME, &PASSWORD).await?;
+        if !nv.is_login().await? {
+            println!("[-] Login failed");
+            process::exit(1);
+        }
     }
-    println!("{}", raw_html);
+    println!("[+] Login OK");
+    for target in targets {
+        if !target.starts_with("sm") && !target.starts_with("nm") {
+            println!("Video ID must start by 'sm' or 'nm'");
+            continue;
+        }
+        nv.get_video_api_data(target).await?;
+    }
     Ok(())
 }
